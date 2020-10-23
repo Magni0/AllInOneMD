6d245f35f189a2b80f732d66d009d43f3012d79a
Logan Harris <loganrodharris@gmail.com>
2020-10-23 15:58:18 +1100

    fixed test

--- test/test_main.py
+++ test/test_main.py
@@ -6,5 +6,5 @@ import main
 
 class TestAdd(unittest.TestCase):
     def test_add(self):
-        result = add(1, 2)
+        result = main.add(1, 2)
         self.assertEqual(result, 3, msg='add did not bring back 3 when given 1 and 2')
