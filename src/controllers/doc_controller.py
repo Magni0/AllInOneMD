d13aa705cc3cea705c4a60fa70307ab395a2f8cd
Logan Harris <loganrodharris@gmail.com>
2020-11-09 14:12:25 +1100

    added routes for crud resource

---
+++ src/controllers/doc_controller.py
@@ -0,0 +1,28 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/md", methods=["GET"])
+def doc_index():
+    # retrive all md documents
+    pass
+
+@app.route("/md", methods=["POST"])
+def doc_create():
+    # create new doc
+    pass
+
+@app.route("/md/<int:id>", methods=["GET"])
+def doc_retrive(id):
+    #get single doc
+    pass
+
+@app.route("/md/<int:id>", methods=["PUT", "PATCH"])
+def doc_update(id):
+    # update a document
+    pass
+
+@app.route("/md/<int:id>", methods=["DELETE"])
+def doc_delete(id):
+    # delete a document
+    pass
