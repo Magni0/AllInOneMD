653650da5df264afbbb01cb3e830f1acdd7f7a4c
Logan Harris <loganrodharris@gmail.com>
2021-02-22 16:22:56 +1100

    added internal documentation for default_settings.py

--- src/default_settings.py
+++ src/default_settings.py
@@ -1,11 +1,19 @@
 import os
 
 class Config(object):
+
+    """these config settings are applied no matter the enviroment.
+    """
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
 
     @property
     def SQLALCHEMY_DATABASE_URI(self):
+
+        """gets the database infomation from the .env file
+        """
+
         value = os.environ.get("DB_URI")
 
         if not value:
@@ -14,11 +22,23 @@ class Config(object):
         return value
 
 class DevelopmentConfig(Config):
+    
+    """applies config for dev enviroment
+    """
+
     DEBUG = True
 
 class ProductionConfig(Config):
+
+    """applies config for production enviroment
+    """
+
     @property
     def JWT_SECRET_KEY(self):
+        
+        """gets JWT_SECRET_KEY from .env
+        """
+
         value = JWT_SECRET_KEY
 
         if not value:
@@ -27,6 +47,10 @@ class ProductionConfig(Config):
         return value
 
 class TestingConfig(Config):
+
+    """applies config for testing enviroment
+    """
+
     TESTING = True
 
 enviroment = os.environ.get("FLASK_ENV")
