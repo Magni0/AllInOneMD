110c489e4614087e09d2c04f23b2600cbe52a20d
Logan Harris <loganrodharris@gmail.com>
2021-02-23 18:02:29 +1100

    prepared doc_controller for form refactor

--- src/controllers/doc_controller.py
+++ src/controllers/doc_controller.py
@@ -5,79 +5,91 @@ from schemas.DocSchema import doc_schema, docs_schema
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required
 from services.decorators import auth_decorator
+from flask_login import login_required
+
 md = Blueprint('document', __name__, url_prefix="/document")
 
 @md.route("/", methods=["GET"])
-@jwt_required
-@auth_decorator
+# @jwt_required
+# @auth_decorator
+@login_required
 def doc_index(user=None):
     
     """reterive all md documents"""
     
-    docs = Document.query.all()
-    return jsonify(docs_schema.dump(docs))
+    pass
+    # docs = Document.query.all()
+    # return jsonify(docs_schema.dump(docs))
 
 @md.route("/", methods=["POST"])
-@jwt_required
-@auth_decorator
+# @jwt_required
+# @auth_decorator
+@login_required
 def doc_create(user=None):
     
     """create new doc"""
 
-    doc_fields = doc_schema.load(request.json)
+    pass
+    # doc_fields = doc_schema.load(request.json)
 
-    new_doc = Document()
-    new_doc.docname = doc_fields["docname"]
+    # new_doc = Document()
+    # new_doc.docname = doc_fields["docname"]
 
-    user.documents.append(new_doc)
+    # user.documents.append(new_doc)
     
-    db.session.add(new_doc)
-    db.session.commit()
+    # db.session.add(new_doc)
+    # db.session.commit()
     
-    return jsonify(doc_schema.dump(new_doc))
+    # return jsonify(doc_schema.dump(new_doc))
 
 @md.route("/<int:id>", methods=["GET"])
-@jwt_required
-@auth_decorator
+# @jwt_required
+# @auth_decorator
+@login_required
 def doc_retrive(id, user=None):
     
     """get single doc"""
 
-    doc = Document.query.get(id)
-    return jsonify(doc_schema.dump(doc))
+    pass
+    # doc = Document.query.get(id)
+    # return jsonify(doc_schema.dump(doc))
 
 @md.route("/<int:id>", methods=["PUT", "PATCH"])
-@jwt_required
-@auth_decorator
+# @jwt_required
+# @auth_decorator
+@login_required
 def doc_update(id, user=None):
 
     """update a document"""
 
-    doc_fields = doc_schema.load(request.json)
+    pass
+    # doc_fields = doc_schema.load(request.json)
     
-    docs = Document.query.filter_by(id=id, user_id=user.id)
+    # docs = Document.query.filter_by(id=id, user_id=user.id)
     
-    if docs.count() != 1:
-        return abort(401, description="Unauthorized to update this document")
+    # if docs.count() != 1:
+    #     return abort(401, description="Unauthorized to update this document")
 
-    docs.update(doc_fields)
-    db.session.commit()
+    # docs.update(doc_fields)
+    # db.session.commit()
 
-    return jsonify(doc_schema.dump(docs[0]))
+    # return jsonify(doc_schema.dump(docs[0]))
 
 @md.route("/<int:id>", methods=["DELETE"])
-@jwt_required
-@auth_decorator
+# @jwt_required
+# @auth_decorator
+@login_required
 def doc_delete(id, user=None):
     
     """delete a document"""
 
-    doc = Document.query.get(id)
+    pass
+    # doc = Document.query.get(id)
     
-    if not doc:
-        return abort(400)
+    # if not doc:
+    #     return abort(400)
     
-    db.session.delete(doc)
-    db.session.commit()
+    # db.session.delete(doc)
+    # db.session.commit()
 
-    return jsonify(doc_schema.dump(doc))
+    # return jsonify(doc_schema.dump(doc))
