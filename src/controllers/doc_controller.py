c9e270754a969f492782d46078f2d8e0d5fc2d84
Logan Harris <loganrodharris@gmail.com>
2021-03-01 14:26:58 +1100

    updated endpoints in doc_controller

--- src/controllers/doc_controller.py
+++ src/controllers/doc_controller.py
@@ -2,7 +2,7 @@ from models.Document import Document
 from models.Authentication import User
 from main import db
 from schemas.DocSchema import doc_schema, docs_schema
-from flask import Blueprint, request, jsonify, render_template, redirect, url_for, abort
+from flask import Blueprint, request, render_template, redirect, url_for, abort
 from flask_login import login_required, current_user
 
 md = Blueprint('document', __name__, url_prefix="/document")
@@ -15,8 +15,6 @@ def doc_index():
 
     docs = Document.query.filter_by(user_id=current_user.get_id())
     return render_template("doc-index.html", docs=docs)
-    # docs = Document.query.all()
-    # return jsonify(docs_schema.dump(docs))
 
 @md.route("/create", methods=["POST"])
 @login_required
@@ -46,20 +44,6 @@ def doc_create():
 
     return render_template("doc-edit.html", file_name=file_name)
 
-    # ---------------------------------------------------------------
-
-    # doc_fields = doc_schema.load(request.json)
-
-    # new_doc = Document()
-    # new_doc.docname = doc_fields["docname"]
-
-    # user.documents.append(new_doc)
-    
-    # db.session.add(new_doc)
-    # db.session.commit()
-    
-    # return jsonify(doc_schema.dump(new_doc))
-
 @md.route("/edit/<int:id>", methods=["GET"])
 @login_required
 def doc_edit(id):
@@ -68,28 +52,6 @@ def doc_edit(id):
 
     pass
     # return render_template("doc-edit.html", content=content)
-
-    # doc = Document.query.get(id)
-    # return jsonify(doc_schema.dump(doc))
-
-# @md.route("/<int:id>", methods=["PUT", "PATCH"])
-# @login_required
-# def doc_update(id):
-
-#     """update a document"""
-
-#     pass
-#     # doc_fields = doc_schema.load(request.json)
-    
-#     # docs = Document.query.filter_by(id=id, user_id=user.id)
-    
-#     # if docs.count() != 1:
-#     #     return abort(401, description="Unauthorized to update this document")
-
-#     # docs.update(doc_fields)
-#     # db.session.commit()
-
-#     # return jsonify(doc_schema.dump(docs[0]))
 
 @md.route("/delete/<int:id>", methods=["DELETE"])
 @login_required
@@ -100,20 +62,18 @@ def doc_delete(id):
     pass
     return redirect(url_for("document.doc_index"))
 
-    # doc = Document.query.get(id)
-    
-    # if not doc:
-    #     return abort(400)
-    
-    # db.session.delete(doc)
-    # db.session.commit()
-
-    # return jsonify(doc_schema.dump(doc))
-
-@md.route("/upload", methods=["GET"])
+@md.route("/discard/<int:id>", methods=["GET"])
 @login_required
-def doc_upload():
+def doc_discard(id):
     
-    """uploads an edited md file to s3 bucket"""
+    """opens an md file to edit"""
+
+    pass
+
+@md.route("/save/<int:id>", methods=["POST"])
+@login_required
+def doc_save(id):
+
+    """updates file in s3 bucket"""
 
     pass
