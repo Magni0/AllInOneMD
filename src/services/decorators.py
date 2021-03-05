71a075d56af0377ced57d057dea574ca06368ad5
Logan Harris <loganrodharris@gmail.com>
2021-03-05 15:07:41 +1100

    tested doc_controller endpoints: they now work

---
+++ src/services/decorators.py
@@ -0,0 +1,13 @@
+
+
+# def decorator_template(func):
+#     @wraps(func)
+#     def wrapper(*args, **kwargs):
+        
+#         # code here for before function
+
+#         return func(*args, user=user, **kwargs)
+    
+#     # code here for arfter function
+
+#     return wrapper
